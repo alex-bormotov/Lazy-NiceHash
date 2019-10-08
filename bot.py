2fc76fe5b6064cb5f77c13d25f8bae63b45f0354
Alex Bormotov <alex.bormotov.work@gmail.com>
2019-10-08 09:33:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,11 +20,14 @@ secret = get_config()["nicehahs_api_secret"]
 private_api = nicehash.private_api(host, organisation_id, key, secret)
 public_api = nicehash.public_api(host, True)
 exchange_info = public_api.get_exchange_markets_info()
+
 last_start = datetime.utcnow()
 
 
 def discord_send_message(notification):
+
     webhookurl = get_config()["discord_webhook_url"]
+
     formdata = (
         '------:::BOUNDARY:::\r\nContent-Disposition: form-data; name="content"\r\n\r\n'
         + notification
@@ -51,22 +54,34 @@ def discord_send_message(notification):
         pass
 #        print((str(e)))
 
+def get_balance(coin):
+    # BTC = 0
+    # XRP = 2
+    return private_api.get_accounts()[coin]["balance"]
+
 
 def make_trade():
-    discord_send_message(f'BTC balance before trade is {private_api.get_accounts()[0]["balance"]}')
-    discord_send_message(f'XRP balance before trade is {private_api.get_accounts()[2]["balance"]}')
-    new_buy_market_order = private_api.create_exchange_buy_market_order(exchange_info['symbols'][1]['symbol'], private_api.get_accounts()[0]["balance"])
+
+    discord_send_message(f'BTC balance before trade is {get_balance(0)}')
+    discord_send_message(f'XRP balance before trade is {get_balance(2)}')
+
+    try:
+        new_buy_market_order = private_api.create_exchange_buy_market_order(exchange_info['symbols'][1]['symbol'], private_api.get_accounts()[0]["balance"])
+    except Exception as e:
+        discord_send_message(str(e))
+
     discord_send_message(f'Bought {new_buy_market_order["executedQty"]} XRP')
-    discord_send_message(f'Total XRP balance after trade is {private_api.get_accounts()[2]["balance"]}')
+    discord_send_message(f'Total XRP balance after trade is {get_balance(2)}')
 
 
 def main():
-    discord_send_message("Starting ...")
-    discord_send_message('Total start balances are:')
-    discord_send_message(f'{private_api.get_accounts()[0]["balance"]} BTC')
-    discord_send_message(f'{private_api.get_accounts()[2]["balance"]} XRP')
 
     global last_start
+
+    discord_send_message("Starting ...")
+
+    if get_balance(0) > 0:
+        make_trade()
 
     while True:
         if last_start + timedelta(hours=float(get_config()["exchange_period_hours"])) < datetime.utcnow():
@@ -77,6 +92,7 @@ def main():
         else:
             time.sleep(3600)
             continue
+
 
 
 
